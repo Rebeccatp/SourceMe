ea45f1ecaf8378616600b28d8b2799429a7419c0
Rebeccatp <2004556C@student.tp.edu.sg>
2022-01-10 16:09:17 +0800

    Added a account class file with some attribute

---
+++ maven.SourceMe/src/main/java/com/sddevops/maven/SourceMe/Account.java
@@ -0,0 +1,12 @@
+package com.sddevops.maven.SourceMe;
+
+public class Account {
+	private int id;
+	private String role;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+}
