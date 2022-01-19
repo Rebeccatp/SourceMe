1507e39627fc270cf78d7f1ed925c864c20531c0
Rebeccatp <2004556C@student.tp.edu.sg>
2022-01-20 02:23:42 +0800

    testing

--- SourceMe/src/main/java/createTutorial.java
+++ SourceMe/src/main/java/createTutorial.java
@@ -32,7 +32,7 @@ public class createTutorial extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter();
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -42,20 +42,19 @@ public class createTutorial extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 		//Step 1: Initialize a PrintWriter object to return the html values via the response
-		PrintWriter out = response.getWriter(); 
+		PrintWriter out = response.getWriter();
 		//Step 2: retrieve the four parameters from the request from the web form
 		String n = request.getParameter("title");
 		String p = request.getParameter("content");
 		//Step 3: attempt connection to database using JDBC, you can change the username and password accordingly using the phpMyAdmin > User Account dashboard
 		try {
 		 Class.forName("com.mysql.jdbc.Driver");
-		 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sourceMe", "root", "password");
+		 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sourceme", "root", "password");
 		//Step 4: implement the sql query using prepared statement (https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html)
-		 PreparedStatement ps = con.prepareStatement("insert into TUTORIALS values(?,?,?)");
+		 PreparedStatement ps = con.prepareStatement("insert into tutorials values(?,?)");
 		//Step 5: parse in the data retrieved from the web form request into the prepared statement accordingly
-		 ps.setInt(1, 0);
-		 ps.setString(2, n);
-		 ps.setString(3, p);
+		 ps.setString(1, n);
+		 ps.setString(2, p);
 		//Step 6: perform the query on the database using the prepared statement
 		 int i = ps.executeUpdate();
 		//Step 7: check if the query had been successfully execute, return “You are successfully registered” via the response
