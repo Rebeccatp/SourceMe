21f7be8d94bb0943dfc5635802d0059c9c710c40
Rebeccatp <2004556C@student.tp.edu.sg>
2022-02-20 23:45:25 +0800

    implemented mockito for junit

--- SourceMe/src/test/java/tutorialCollectionTest.java
+++ SourceMe/src/test/java/tutorialCollectionTest.java
@@ -5,12 +5,18 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 class tutorialCollectionTest {
 
 	
 	private Tutorial tutorial;
+	private Tutorial tutorial2;
+	private TutorialCollection mockTutorialCollection = mock(TutorialCollection.class);
+	
 	private Integer tutorialId;
 	private Integer deleteId;
 	private Integer errorId;
@@ -26,20 +32,20 @@ class tutorialCollectionTest {
 	private String usernameerr;
 	private String passworderr;
 	private Integer setId;
+	private String role;
 
 	private String classname;
 	private String classnameErr;
-//	Tutorial connect = Mockito.mock(Tutorial.class);
-//	private Connection connection;
 	
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		tutorial = new Tutorial(0, null,null);
-		tutorialId = 4;
-		deleteId = 32;
+		tutorial2 = new Tutorial(mockTutorialCollection);
+		tutorialId = 40;
+		deleteId = 50;
 		errorId = null;
-		setId = 1;
+		setId = 42;
 		t1 = "T1";
 		t2 = "T2";
 		t3 = "T3";
@@ -53,6 +59,7 @@ class tutorialCollectionTest {
 		passworderr = "testing";
 		classname = "com.mysql.jdbc.Driver";
 		classnameErr= "tetsing";
+		role = "Admin";
 	}
 
 	@AfterEach
@@ -62,84 +69,145 @@ class tutorialCollectionTest {
 
 	@Test
 	void testGetConnection() {
+		
+		//Act (Get the correct connection parameters)
 		var connection = tutorial.getConnection(jdbcURL, jdbcUsername, jdbcPassword,classname);
+		//Assert
 		assertNotNull(connection);
+		
+		//Act (Get the incorrect username and password (SQLException))
 		var connectionError = tutorial.getConnection(jdbcURL, usernameerr, passworderr,classname);
+		//Assert
 		assertNull(connectionError);
+		
+		//Act (Get the incorrect classname (ClassNotFoundException))
 		var connectionError2 = tutorial.getConnection(jdbcURL, jdbcUsername, jdbcPassword, classnameErr);
+		//Assert
 		assertNull(connectionError2);
 	}
 	
 
 	@Test
 	void testGetAllTutorials() {
-		//Act
+		
+		//Act (Get all the tutorial)
 		List<Tutorial> testTc = tutorial.getAllTutorials();
 		//Assert
-		assertFalse(testTc.isEmpty());
-		
-//		Mockito.doReturn(connection).when(connect).getConnection(jdbcURL, jdbcUsername, jdbcPassword, classname);
-//		List<Tutorial>  testTc =connect.getAllTutorials();
-//		assertFalse(testTc.isEmpty());
-//		Mockito.when(connect.getConnection(jdbcURL, jdbcUsername, jdbcPassword, classname)).thenReturn(connection);
+		assertFalse(testTc.isEmpty());	
 		
 	}
 
 	@Test
 	void testGetTutorialById() {
-		//Act
+		
+		//Act (Get tutorial by id)
 		var tutorialById = tutorial.getTutorialById(tutorialId);
-		//Assert
+		//Assert (check that is it not null)
 		assertNotNull(tutorialById);
+		
+		//Act (Get Tutorial by id(0))
 		var tutorialBy0 = tutorial.getTutorialById(0);
-		//Assert
+		//Assert (check that it is null)
 		assertNull(tutorialBy0);
 	}
 
 	@Test
 	void testCreateTutorial() {
+		
 		//	Assert the number of tutorials
 		List<Tutorial> testTc = tutorial.getAllTutorials();
 		assertFalse(testTc.isEmpty());
 		int beforeCreate = testTc.size();
+		
 		// Act
-		tutorial.createTutorial(t1, c1);
-		// Assert
-		assertEquals(tutorial.getAllTutorials().size(), beforeCreate +1);
-		var createError = tutorial.createTutorial(null, null);
-		assertFalse(createError);
+		when(mockTutorialCollection.ifAdmin(role)).thenReturn(true);
+		
+		//Act & Assert (Check if return true value)
+		assertTrue(tutorial2.createTutorial(t1, c1,role));
+		
+		// Assert (Check if the size increased)
+		assertEquals(tutorial.getAllTutorials().size(), beforeCreate +1);		
+		
+		//check if mocktutorial collection is being used
+		verify(mockTutorialCollection).ifAdmin(role);
+	}
+	
+	@Test
+	void testCreateTutorialError() {		
+		
+		//	Assert the number of tutorials
+		List<Tutorial> testTc = tutorial.getAllTutorials();
+		assertFalse(testTc.isEmpty());
+		int beforeCreate = testTc.size();
+		
+		// Act
+		when(mockTutorialCollection.ifAdmin(role)).thenReturn(true);
+		
+		//Act & Assert (Check if return false value and if the size remain the same )
+		assertFalse(tutorial2.createTutorial(null, null, role));
+		assertEquals(tutorial.getAllTutorials().size(), beforeCreate);
+		verify(mockTutorialCollection).ifAdmin(role);
+		
+		// Act & Assert (Check if return false value with incorrect role)
+		assertFalse(tutorial2.createTutorial(t1, c1, null));
 	}
 
 	@Test
 	void testUpdateTutorialById() {
+		when(mockTutorialCollection.ifAdmin(role)).thenReturn(true);
 		//pass call update (parameter1)
 		var tutorialById = tutorial.getTutorialById(tutorialId);
-		tutorial.updateTutorialById(tutorialId, t3, c3);
-		
+		assertTrue(tutorial2.updateTutorialById(tutorialId, t3, c3, role));
 		// call retrieve record to check para
 		assertNotSame(tutorialById,tutorial.getTutorialById(tutorialId));
-		var updateError = tutorial.updateTutorialById(errorId, t2, c1);
-		assertFalse(updateError);
+		verify(mockTutorialCollection).ifAdmin(role);
+	}
+	
+	@Test
+	void testUpdateTutorialByIdError() {
+		when(mockTutorialCollection.ifAdmin(role)).thenReturn(true);
+		//pass call update (parameter1)
+		var tutorialById = tutorial.getTutorialById(tutorialId);
+		// call retrieve record to check para
+		assertFalse(tutorial2.updateTutorialById(errorId, t2, c1,role));
+		verify(mockTutorialCollection).ifAdmin(role);
+		
+		// Act & Assert (Check if return false value with incorrect role)
+		assertFalse(tutorial2.updateTutorialById(tutorialId, t3, c3, null));
 	}
 
 	@Test
 	void testDeleteTutorialById() {
+		when(mockTutorialCollection.ifAdmin(role)).thenReturn(true);
 		List<Tutorial> testTc = tutorial.getAllTutorials();
 		assertFalse(testTc.isEmpty());
 		int beforeDelete = testTc.size();
 		var tutorialById = tutorial.getTutorialById(deleteId);
 		
 		//delete tutorial
-		var deletedTutorial = tutorial.deleteTutorialById(deleteId);
-		
+		//var deletedTutorial = tutorial2.deleteTutorialById(deleteId,role);
+		assertTrue(tutorial2.deleteTutorialById(deleteId,role));
 		// Check size before and after delete
 		assertEquals(tutorial.getAllTutorials().size(), beforeDelete-1);
 		
-		assertTrue(deletedTutorial);
+
 		// Check deleted tutorial
 		assertNotSame(tutorialById,tutorial.getTutorialById(deleteId));
-		var deleteerror = tutorial.deleteTutorialById(errorId);
-		assertFalse(deleteerror);
+		verify(mockTutorialCollection).ifAdmin(role);
+	}
+	
+	@Test
+	void testDeleteTutorialByIdError() {
+		when(mockTutorialCollection.ifAdmin(role)).thenReturn(true);
+		List<Tutorial> testTc = tutorial.getAllTutorials();
+		assertFalse(testTc.isEmpty());
+		int beforeDelete = testTc.size();
+		assertFalse(tutorial2.deleteTutorialById(errorId, role));
+		assertEquals(tutorial.getAllTutorials().size(), beforeDelete);
+		verify(mockTutorialCollection).ifAdmin(role);
+		
+		// Act & Assert (Check if return false value with incorrect role)
+		assertFalse(tutorial2.deleteTutorialById(deleteId, null));
 	}
 	
 	@Test
