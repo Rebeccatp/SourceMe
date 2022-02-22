d4739ffde09e63a47c378530bf9d8ebb9f49ff40
Zannlxe <2004356H@student.tp.edu.sg>
2022-02-22 14:17:48 +0800

    remove useless lines

--- SourceMe/src/test/java/com/sddevops/SourceMe/AnswerTest.java
+++ SourceMe/src/test/java/com/sddevops/SourceMe/AnswerTest.java
@@ -16,164 +16,134 @@ public class AnswerTest {
 	@Test
 	  public void aCreateAnswer() throws InterruptedException {
 		//Starting page
-		driver.get("http://localhost:8090/SourceMe/home");
+		driver.get("http://localhost:8080/SourceMe/home");
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-		Thread.sleep(1800);
 		
 		//Click on "All Question" button
 		driver.findElement(By.name("questions")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
 		//Click on �View Answer� button
 	    driver.findElement(By.linkText("View Answer")).click();
-	    Thread.sleep(1800);
 	    
 		//Click on �Add your Answer� button
 	    driver.findElement(By.name("btn-add-answer")).click();
-	    Thread.sleep(1800);
 	    
 		//Enter username
 	    driver.findElement(By.name("userName")).sendKeys("Ad");
 	    (js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
 		//Enter password
 	    driver.findElement(By.name("password")).sendKeys("admin@123");
 	    (js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
 		//Click on �login� button
 	    driver.findElement(By.id("login")).click();
-	    Thread.sleep(1800);
 	    
 		//Click on �Home� button
 	    driver.findElement(By.linkText("Home")).click();
 	    (js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
 		//Click on �All Question� button
 	    driver.findElement(By.name("questions")).click();
 	    (js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
-	    
+                                 	    
 		//Click on �View Answer� button
 	    driver.findElement(By.linkText("View Answer")).click();
-	    Thread.sleep(1800);
 	    
 		//Click on �Add your Answer� button
 	    driver.findElement(By.name("btn-add-answer")).click();
-	    Thread.sleep(1800);
 	    
 		//Enter the answer
 	    driver.findElement(By.name("answer")).sendKeys("selenium testing");
 	    (js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
 		//Click on �Submit Answer� button
 	    driver.findElement(By.id("submitAnswer")).click();
 	    (js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
-	}
+ 	}
 	
   @Test
   public void bGetAnswerByQnsId() throws InterruptedException {
 	  	//Starting page
-	  	driver.get("http://localhost:8090/SourceMe/home");
+	  	driver.get("http://localhost:8080/SourceMe/home");
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-		Thread.sleep(1800);
 		
 		//Click on �All Question� button
 		driver.findElement(By.name("questions")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
-		
+    		
 		//Click on �View Answer� button 
 	    driver.findElement(By.linkText("View Answer")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
   }
   
   @Test
   public void cGetAnswerById() throws InterruptedException {
 	  	//Starting page
-	  	driver.get("http://localhost:8090/SourceMe/home");
+	  	driver.get("http://localhost:8080/SourceMe/home");
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-		Thread.sleep(1800);
 		
 		//Click on �All Question� button
 		driver.findElement(By.name("questions")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 		
 		//Click on �View Answer� button 
 	    driver.findElement(By.linkText("View Answer")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
 	    //Click on �edit� button 
 	    driver.findElement(By.name("editAnswer")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
   }
   
   @Test
   public void dUpdateAnswerById() throws InterruptedException {
 	  	//Starting page
-	  	driver.get("http://localhost:8090/SourceMe/home");
+	  	driver.get("http://localhost:8080/SourceMe/home");
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-		Thread.sleep(1800);
 		
 		//Click on �All Question� button 
 		driver.findElement(By.name("questions")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
 		//Click on �View Answer� button 
 	    driver.findElement(By.linkText("View Answer")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 		
 		//Click on �edit� button 
 	    driver.findElement(By.name("editAnswer")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
 	    
 	    //Clear previous input
 	    driver.findElement(By.name("answer")).clear();
 		
 		//Make changes to answer
 	    driver.findElement(By.name("answer")).sendKeys("selenium edited text");
-	    Thread.sleep(1800);
 		
 		//Click on �Edit Answer� button
 	    driver.findElement(By.id("editAnswer")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
   }
   
   @Test
   public void eDeleteAnswerById() throws InterruptedException {
 	  	//Starting page
-	  	driver.get("http://localhost:8090/SourceMe/home");
+	  	driver.get("http://localhost:8080/SourceMe/home");
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-		Thread.sleep(1800);
 		
 		//Click on �All Question� button 
 		driver.findElement(By.name("questions")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);  
 		
 	    //Click on �View Answer� button 
 	    driver.findElement(By.linkText("View Answer")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800); 
 						
 		//Click on �Delete� button
 	    driver.findElement(By.name("btnDel")).click();
 		(js).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-	    Thread.sleep(1800);
   }
   
   @BeforeTest
