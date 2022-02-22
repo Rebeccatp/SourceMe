b86b0c18abc25bbd331a1d418a2b2bbda83163a6
jaslynyap <2005309F@student.tp.edu.sg>
2022-02-23 01:55:50 +0800

    changed deleteId

--- SourceMe/src/test/java/AnswerCollectionTest.java
+++ SourceMe/src/test/java/AnswerCollectionTest.java
@@ -52,7 +52,7 @@ class AnswerCollectionTest {
 		questionId = 5;
 		answer = new Answer(0,0,null,null);
 		updateId = 84;
-		deleteId = 272;
+		deleteId = 273;
 		postBy = "jaslynylh";
 		answers = "123";
 		jdbcURL = "jdbc:mysql://localhost:3306/sourceme";
